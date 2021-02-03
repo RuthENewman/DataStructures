f04d25e2d38749ffac7000d48d34942614f2b241
Ruth Newman <ruthnewman@gmail.com>
2021-02-03 23:56:26 +0000

    solution

--- reverseAString.js
+++ reverseAString.js
@@ -1,3 +1,11 @@
 function reverse(str) {
-    
+    const stringArray = [];
+    str.split("").forEach((letter) => {
+        stringArray.unshift(letter);
+    })
+    return stringArray.join("");
 }
+
+const result = reverse("example");
+
+console.log(result);
