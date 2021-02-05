5ad4678b323de5d0cd9a833b30f1f61ec1f1629d
Ruth Newman <ruthnewman@gmail.com>
2021-02-05 23:54:28 +0000

    for loop version

--- reverseAString.js
+++ reverseAString.js
@@ -1,14 +1,31 @@
+const canBeReversed = (str) => (str && (str.length > 1) && (typeof str === 'string'));
+
 function reverse(str) {
-if(!str || str.length <= 1 || typeof str !== 'string') {
+    if (canBeReversed(str)) {
+        const backwardsArray = [];
+        str.split("").forEach((letter) => {
+            backwardsArray.unshift(letter);
+        })
+        return backwardsArray.join("");
+    }
     return str;
-}
-    const stringArray = [];
-    str.split("").forEach((letter) => {
-        stringArray.unshift(letter);
-    })
-    return stringArray.join("");
 }
 
 const result = reverse("example");
 
 console.log(result);
+
+function forLoopReverse(str) {
+    if (canBeReversed(str)) {
+        const backwardsArray = [];
+        for(let i = str.length - 1; i >= 0; i--) {
+            backwardsArray.push(str[i]);
+        }
+        return backwardsArray.join("");
+    }
+    return str;
+}
+
+const loopResult = forLoopReverse("another example");
+
+console.log(loopResult);
