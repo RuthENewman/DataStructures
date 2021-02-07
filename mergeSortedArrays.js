021c5218ff7b32306c702af71fa744fdd55dbd78
Ruth Newman <ruthnewman@gmail.com>
2021-02-07 00:18:50 +0000

    merge sorted arrays solution

--- mergeSortedArrays.js
+++ mergeSortedArrays.js
@@ -1,6 +1,35 @@
-const mergeSortedArrays = () => {
+const mergeSortedArrays = (firstArray, secondArray) => {
+    if (firstArray.length === 0) {
+        return firstArray;
+    } else if (secondArray.length === 0) {
+        return secondArray;
+    }
+    
+    const mergedArray = [];
+    let i = 0;
+    let j = 0;
 
+    let firstArrayItem = firstArray[i];
+    let secondArrayItem = secondArray[j];
+
+    // Check input
+
+    while (firstArrayItem || secondArrayItem) {
+        if (!secondArrayItem || firstArrayItem < secondArrayItem) {
+            mergedArray.push(firstArrayItem);
+            i++;
+            firstArrayItem = firstArray[i];
+        } else {
+            mergedArray.push(secondArrayItem);
+            j++;
+            secondArrayItem = secondArray[j];
+        }
+    }
+    
+    return mergedArray;
 }
 
-mergeSortedArrays([0,3,4,31], [4,6,30]);
+const result = mergeSortedArrays([0,3,4,31], [4,6,30]);
 // returns [0,3,4,4,6,30,31];
+
+console.log(result);
