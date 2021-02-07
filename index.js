185e8f2a1d36798a318c4ec25ac0fa1708e2dc27
Ruth Newman <ruthnewman@gmail.com>
2021-02-07 23:46:08 +0000

    arrays

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const strings = ['a', 'b', 'c', 'd'];
 const numbers = [1,2,3,4,5];
 
-strings.push('e');
-console.log('after push', strings);
+strings.push('e'); // 0{1}
+console.log('after push', strings); 
 
-strings.pop();
-console.log('after pop', strings);
+strings.pop(); // 0(1)
+console.log('after pop', strings); 
 
-strings.unshift('z');
-console.log('after unshift', strings);
+strings.unshift('z'); // 0(n)
+console.log('after unshift', strings); 
 
-strings.splice(2,0, 'y');
-console.log('after splice', strings);
+strings.splice(2,0, 'y'); // 0(n)
+console.log('after splice', strings); 
